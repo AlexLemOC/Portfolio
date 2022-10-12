50de84794ff65334ddd3dab2fc578916f1b88b94
Alexis <alexismichellemee@gmail.com>
2022-10-12 18:47:27 +0200

    ...

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import SimplexNoise from "simplex-noise";
 import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap-trial/ScrollTrigger"
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 import "./base.css";
 import "./style.css";
 
